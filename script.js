9860e807bedf53b5dd58ff01bb682dc87e9b9999
sheetedSquid <yourname@example.com>
2024-01-13 10:42:33 +0200

    finished all UI and its functionality

--- script.js
+++ script.js
@@ -2,15 +2,17 @@ let counterPlayer = 0;
 let counterComputer = 0;
 let counterRound = 0;
 
-let button = document.querySelector('button');
+let body = document.querySelector('body');
 let playerResult = document.querySelector('#player-result');
 let computerResult = document.querySelector('#computer-result');
 let roundResult = document.querySelector('#round-result');
 let computerChoiceDisplay = document.querySelector('#computer-choice');
+let uselessSign = document.querySelector('#useless-sign');
 let rock1 = document.querySelector('#rock');
 let paper2 = document.querySelector('#paper');
 let scissors3 = document.querySelector('#scissors');
 let getUserChoice;
+let winnerDisplay = document.createElement('div');
 
 playerResult.textContent = `Player: ${counterPlayer}`;
 computerResult.textContent = `Computer: ${counterComputer}`;
@@ -21,6 +23,11 @@ rock1.addEventListener('click', () => {
     getComputer = getComputerChoice();
     let gameResult = game(playRound(getUserChoice, getComputer));
     console.log(gameResult);
+    if (counterRound >= 5) {
+        removeAll();
+        winnerDisplay.textContent = `${endGame(counterPlayer, counterComputer)}`;
+        body.appendChild(winnerDisplay);
+    }
 });
 
 paper2.addEventListener('click', () => {
@@ -28,6 +35,11 @@ paper2.addEventListener('click', () => {
     getComputer = getComputerChoice();
     let gameResult = game(playRound(getUserChoice, getComputer));
     console.log(gameResult);
+    if (counterRound >= 5) {
+        removeAll();
+        winnerDisplay.textContent = `${endGame(counterPlayer, counterComputer)}`;
+        body.appendChild(winnerDisplay);
+    }
 });
 
 scissors3.addEventListener('click', () => {
@@ -35,7 +47,17 @@ scissors3.addEventListener('click', () => {
     getComputer = getComputerChoice();
     let gameResult = game(playRound(getUserChoice, getComputer));
     console.log(gameResult);
+    if (counterRound >= 5) {
+        removeAll();
+        winnerDisplay.textContent = `${endGame(counterPlayer, counterComputer)}`;
+        body.appendChild(winnerDisplay);
+    }
 });
+
+if (counterRound === 5) {
+    console.log('yoyoy')
+    playerResult.remove();
+}
 
 function getRandom() {
     return (Math.floor(Math.random() * 3) + 1);
@@ -75,6 +97,8 @@ function playRound(userChoice, computerChoice) {
     };
 }
 
+
+
 function game(round) {
     switch(round) {
         case "win":
@@ -94,4 +118,22 @@ function game(round) {
             roundResult.textContent = `Round: ${counterRound}`;
             return 2;
     }  
+}
+
+function endGame(player, computer) {
+    if (player > computer) {
+        return "YOU WON"
+    } else if (player < computer) {
+        return "COMPUTER WON, YOU LOST"
+    } else if (player === computer) {
+        return "IT'S a DRAW"
+    }
+}
+
+function removeAll() {
+    roundResult.remove();
+    uselessSign.remove();
+    rock1.remove();
+    paper2.remove();
+    scissors3.remove();
 }
